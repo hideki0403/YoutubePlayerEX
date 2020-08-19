7ddee6054bafa9380dd754e911f2cf9614bcaaa3
yukineko <hideki030403@gmail.com>
2020-08-20 00:09:57 +0900

    Security update

--- build/build.js
+++ build/build.js
@@ -1,31 +1,8 @@
 var packager = require('electron-packager')
 var src = '../app'
 var config = require(src + '/package.json')
-const builder = require('electron-builder');
 
 var version = config.version
-
-/*
-builder.build({
-    platform: 'win',
-    config: {
-        'directories': {
-            'app': '../app',
-            'output': '../packaged/' + version
-        },
-        'appId': 'ml.hideki0403.ytex',
-        'productName': 'YoutubePlayerEX',
-        'buildVersion': version,
-        'win':{
-            'icon': '../app/src/ytex.ico',
-            'target': {
-                'target': 'portable',
-                'arch': ['x64']
-            }
-        }
-    }
-});
-*/
 
 packager({  
   dir: src,
